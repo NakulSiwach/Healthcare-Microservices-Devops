8428483600ccd591bf346590da59279258de4a91
NakulSiwach <Nakul.Siwach@iiitb.ac.in>
2025-04-25 03:06:25 +0530

    docker compose done with rabbitmq and DB

--- PatientService/src/main/java/com/spe/patientservice/PatientServiceApplication.java
+++ PatientService/src/main/java/com/spe/patientservice/PatientServiceApplication.java
@@ -2,10 +2,12 @@ package com.spe.patientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 //@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PatientServiceApplication {
 
